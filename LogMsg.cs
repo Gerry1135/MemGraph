d87f46031708022db7b142f489a629d29e2798c4
Gerry1135 <gerry@madeupplace.com>
2016-05-15 15:28:40 +0100

    Added LogMsg class

---
+++ LogMsg.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+using UnityEngine;
+
+namespace MemGraph
+{
+    public class LogMsg
+    {
+        public StringBuilder buf;
+
+        public LogMsg()
+        {
+            buf = new StringBuilder(65536);
+        }
+
+        public void Flush()
+        {
+            if (buf.Length > 0)
+                MonoBehaviour.print(buf);
+            buf.Length = 0;
+        }
+    }
+}
